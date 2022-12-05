b9f2374c2b9ecb2a88fab442ed4749554ebedb3d
wchristian84 <99511387+wchristian84@users.noreply.github.com>
2022-12-05 18:43:58 +0000

    started current/archived pages, updated task model

--- src/app/list/current-tasks/current-tasks.component.ts
+++ src/app/list/current-tasks/current-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
 
 @Component({
   selector: 'app-current-tasks',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-tasks.component.css']
 })
 export class CurrentTasksComponent implements OnInit {
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor() { } // Inject taskService once it exists
 
   ngOnInit(): void {
   }
